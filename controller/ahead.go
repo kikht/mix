41f9feca7ff7067634c0ea42728c51306511d27d
Vladimir Kikhtenko <kva911@gmail.com>
2019-04-27 00:01:28 +0700

    refactor controller to provide source mutators

---
+++ controller/ahead.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"github.com/kikht/mix"
+
+	"time"
+)
+
+type AheadController struct {
+	Controller
+	ahead  mix.Tz
+	player mix.Player
+	start  time.Time
+	mix    mix.Source
+}
+
+func NewAheadController(sampleRate mix.Tz, player mix.Player) *AheadController {
+	fade := mix.DurationToTz(100*time.Millisecond, sampleRate)
+	return &AheadController{
+		Controller: NewController(fade, sampleRate),
+		ahead:      mix.DurationToTz(300*time.Millisecond, sampleRate),
+		player:     player,
+	}
+}
+
+func (c *AheadController) Action(label string) error {
+	gen, err := c.Controller.Action(label)
+	if err != nil {
+		return err
+	}
+	c.mix = gen(c.mix, c.pos())
+	c.player.Play(c.mix)
+	return nil
+}
+
+func (c *AheadController) Mix() mix.Source {
+	return c.mix
+}
+
+func (c *AheadController) pos() mix.Tz {
+	if c.start.IsZero() {
+		c.start = time.Now()
+		return c.ahead
+	}
+	return mix.DurationToTz(time.Now().Sub(c.start), c.sampleRate) + c.ahead
+}
--- controller/controller.go
+++ controller/controller.go
@@ -1,10 +1,10 @@
 package controller
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/kikht/mix"
+
+	"fmt"
+	"log"
 )
 
 type Ambience mix.Source
@@ -15,34 +15,23 @@ type Music struct {
 }
 
 type Controller struct {
-	sampleRate  mix.Tz
-	ahead, fade mix.Tz
-	start       time.Time
-
-	player mix.Player
+	fade, sampleRate mix.Tz
 
 	ambience map[string]Ambience
 	music    map[string]Music
 	effect   map[string]Effect
 
-	mix *mix.Session
-	cur string
+	lastAmbience string
 }
 
-func NewController(sampleRate mix.Tz, player mix.Player) *Controller {
-	return &Controller{
+func NewController(fade, sampleRate mix.Tz) Controller {
+	return Controller{
+		fade:       fade,
 		sampleRate: sampleRate,
-		ahead:      mix.DurationToTz(300*time.Millisecond, sampleRate),
-		fade:       mix.DurationToTz(100*time.Millisecond, sampleRate),
-		player:     player,
 		ambience:   make(map[string]Ambience),
 		music:      make(map[string]Music),
 		effect:     make(map[string]Effect),
 	}
-}
-
-func (c *Controller) Mix() *mix.Session {
-	return c.mix
 }
 
 func (c *Controller) AddAmbience(label string, sound mix.Source) {
@@ -55,123 +44,6 @@ func (c *Controller) AddMusic(label string, sound mix.Source, after string) {
 
 func (c *Controller) AddEffect(label string, sound mix.Source) {
 	c.effect[label] = Effect(sound)
-}
-
-func (c *Controller) pos() mix.Tz {
-	if c.start.IsZero() {
-		c.start = time.Now()
-		return 0
-	}
-	return mix.DurationToTz(time.Now().Sub(c.start), c.sampleRate)
-}
-
-func (c *Controller) SetAmbience(label string) error {
-	amb, ok := c.ambience[label]
-	if !ok {
-		return fmt.Errorf("Ambience %s is not found", label)
-	}
-
-	pos := c.pos() + c.ahead - c.fade
-	oldMix := c.mix
-	newMix := mix.NewSession(c.sampleRate)
-	if oldMix != nil {
-		newMix.AddRegion(mix.Region{
-			Source:  oldMix,
-			Begin:   0,
-			Offset:  0,
-			Volume:  1,
-			Length:  pos,
-			FadeOut: c.fade,
-		})
-	}
-	newMix.AddRegion(mix.Region{
-		Source: amb,
-		Begin:  pos,
-		Offset: pos,
-		Volume: 1,
-		FadeIn: c.fade,
-	})
-
-	c.cur = label
-	c.mix = newMix
-	c.player.Play(c.mix)
-	return nil
-}
-
-func (c *Controller) PlayMusic(label string) error {
-	mus, ok := c.music[label]
-	if !ok {
-		return fmt.Errorf("Music %s is not found", label)
-	}
-	ambLabel := mus.After
-	if ambLabel == "" {
-		ambLabel = c.cur
-	}
-	amb, ok := c.ambience[ambLabel]
-	if !ok {
-		return fmt.Errorf("Ambience %s after music %s is not found",
-			ambLabel, label)
-	}
-
-	pos := c.pos() + c.ahead - c.fade
-	oldMix := c.mix
-	newMix := mix.NewSession(c.sampleRate)
-	//Fade out of current ambience
-	if oldMix != nil {
-		newMix.AddRegion(mix.Region{
-			Source:  oldMix,
-			Begin:   0,
-			Offset:  0,
-			Volume:  1,
-			Length:  pos,
-			FadeOut: c.fade,
-		})
-	}
-	//Music itself
-	newMix.AddRegion(mix.Region{
-		Source:  mus,
-		Begin:   pos,
-		Offset:  0,
-		Volume:  1,
-		FadeIn:  c.fade,
-		FadeOut: c.fade,
-	})
-	//Next ambience with fade in
-	eventEnd := pos + mus.Length() - c.fade
-	newMix.AddRegion(mix.Region{
-		Source: amb,
-		Begin:  eventEnd,
-		Offset: eventEnd,
-		Volume: 1,
-		FadeIn: c.fade,
-	})
-
-	c.mix = newMix
-	c.player.Play(c.mix)
-	return nil
-}
-
-func (c *Controller) FireEffect(label string) error {
-	eff, ok := c.effect[label]
-	if !ok {
-		return fmt.Errorf("Music %s is not found", label)
-	}
-
-	pos := c.pos() + c.ahead - c.fade
-	if c.mix == nil {
-		c.mix = mix.NewSession(c.sampleRate)
-	}
-	c.mix.AddRegion(mix.Region{
-		Source:  eff,
-		Begin:   pos,
-		Offset:  0,
-		Volume:  1,
-		FadeIn:  c.fade,
-		FadeOut: c.fade,
-	})
-
-	c.player.Play(c.mix)
-	return nil
 }
 
 func (c *Controller) Actions() [][]string {
@@ -188,12 +60,127 @@ func (c *Controller) Actions() [][]string {
 	return res
 }
 
-func (c *Controller) Action(label string) error {
+func (c *Controller) Ambience(label string) (mix.SourceMutator, error) {
+	amb, ok := c.ambience[label]
+	if !ok {
+		return nil, fmt.Errorf("Ambience %s is not found", label)
+	}
+	c.lastAmbience = label
+
+	mutator := func(cur mix.Source, pos mix.Tz) mix.Source {
+		log.Println("Generating ambience", pos)
+		//TODO: reuse session to prevent allocations
+		next := mix.NewSession(c.sampleRate)
+		if cur != nil {
+			next.AddRegion(mix.Region{
+				Source:  cur,
+				Begin:   0,
+				Offset:  0,
+				Volume:  1,
+				Length:  pos - c.fade,
+				FadeOut: c.fade,
+			})
+		}
+		next.AddRegion(mix.Region{
+			Source: amb,
+			Begin:  pos - c.fade,
+			Offset: pos - c.fade,
+			Volume: 1,
+			FadeIn: c.fade,
+		})
+		return next
+	}
+	return mutator, nil
+}
+
+func (c *Controller) Music(label string) (mix.SourceMutator, error) {
+	mus, ok := c.music[label]
+	if !ok {
+		return nil, fmt.Errorf("Music %s is not found", label)
+	}
+	ambLabel := mus.After
+	if ambLabel == "" {
+		ambLabel = c.lastAmbience
+	}
+	amb, ok := c.ambience[ambLabel]
+	if !ok {
+		return nil, fmt.Errorf("Ambience %s after music %s is not found",
+			ambLabel, label)
+	}
+	c.lastAmbience = label
+
+	mutator := func(cur mix.Source, pos mix.Tz) mix.Source {
+		log.Println("Generating music", pos)
+		//TODO: reuse session to prevent allocations
+		next := mix.NewSession(c.sampleRate)
+		//Fade out of current ambience
+		if cur != nil {
+			next.AddRegion(mix.Region{
+				Source:  cur,
+				Begin:   0,
+				Offset:  0,
+				Volume:  1,
+				Length:  pos - c.fade,
+				FadeOut: c.fade,
+			})
+		}
+		//Music itself
+		next.AddRegion(mix.Region{
+			Source:  mus,
+			Begin:   pos - c.fade,
+			Offset:  0,
+			Volume:  1,
+			FadeIn:  c.fade,
+			FadeOut: c.fade,
+		})
+		//Next ambience with fade in
+		eventEnd := pos + mus.Length() - c.fade
+		next.AddRegion(mix.Region{
+			Source: amb,
+			Begin:  eventEnd,
+			Offset: eventEnd,
+			Volume: 1,
+			FadeIn: c.fade,
+		})
+		return next
+	}
+	return mutator, nil
+}
+
+func (c *Controller) Effect(label string) (mix.SourceMutator, error) {
+	eff, ok := c.effect[label]
+	if !ok {
+		return nil, fmt.Errorf("Music %s is not found", label)
+	}
+
+	mutator := func(cur mix.Source, pos mix.Tz) mix.Source {
+		log.Println("Generating effect", pos)
+		var next *mix.Session
+		next, ok := cur.(*mix.Session)
+		if ok {
+			next = next.Clone().(*mix.Session)
+		} else {
+			next = mix.NewSession(c.sampleRate)
+		}
+		next.AddRegion(mix.Region{
+			Source:  eff,
+			Begin:   pos,
+			Offset:  0,
+			Volume:  1,
+			FadeIn:  c.fade,
+			FadeOut: c.fade,
+		})
+		return next
+	}
+	return mutator, nil
+}
+
+func (c *Controller) Action(label string) (mix.SourceMutator, error) {
 	if _, ok := c.effect[label]; ok {
-		return c.FireEffect(label)
+		return c.Effect(label)
 	} else if _, ok = c.music[label]; ok {
-		return c.PlayMusic(label)
+		return c.Music(label)
 	} else {
-		return c.SetAmbience(label)
+		return c.Ambience(label)
 	}
 }
